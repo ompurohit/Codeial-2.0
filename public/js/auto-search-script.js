bbf31b5142e89913135aab7c6f4873ec7d8776c1
ompurohit <ompurohit353@gmail.com>
2023-07-17 22:24:40 +0530

    autosearch is completed. remaining, put data in label

--- public/js/auto-search-script.js
+++ public/js/auto-search-script.js
@@ -1,12 +1,44 @@
 async function getInputStoke(){
     const keystroke = document.getElementById('label').value;
+    const searchBox = document.getElementById('auto-search-box');
+    const container = document.getElementById('search-container');
+    container.innerHTML = '';
     try{
         const response = await fetch(`/labels/${keystroke}`);
         const result = await response.json();
-        console.log(result);
+
+        const htmlFormatedText = formateText(result);
+    
+        container.innerHTML =  htmlFormatedText;
+        searchBox.classList.remove('hidden');
     }catch (error){
         console.error(error);
-    }
 
-    console.log('text',keystroke);
+    }
 }
+
+/**
+ * parsing arrary data in string and formate it into the html text.
+ * 
+ * @param {*} data Array
+ * @retrun string formate
+ */
+function formateText(data){
+    let output = '';
+    for (let item of data) {
+        output +=
+            `<p class="text-sm text-gray-900 pt-2 transition duration-500 ease-in-out cursor-pointer hover:bg-gray-800 hover:text-white hover:pl-2 label-container" > 
+            ${item.name} 
+         </p>
+         <input type="hidden" class="labels_id" name="labels[]" value="${item._id}" />
+        `;
+    }
+    console.log('ouput',output)
+    return output;
+}
+
+/**
+ * put label value in input tax as label(HTML)
+ * 
+ * 
+ */
--- routes/project.js
+++ routes/project.js
@@ -6,10 +6,10 @@ const projectController = require('../app/Http/controllers/projectController');
 
 
 router.get('/', projectController.index);
-router.get('/:_id', projectController.issue);
 
 router.get('/create', projectController.create);
 router.post('/store', projectController.store);
+router.get('/:_id', projectController.issue);
 
 
 
